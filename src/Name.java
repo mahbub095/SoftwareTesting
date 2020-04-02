a18744020ebfa98c4a531de190b2aa24487e8ff4
Mahbub <mahbub.095@gmail.com>
2020-04-02 19:20:06 +0600

    Site Title Name Search

--- src/Name.java
+++ src/Name.java
@@ -6,24 +6,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Name {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver", "files/geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
-		driver.getTitle();
-		driver.navigate().to("https://www.edureka.co/testing-with-selenium-webdriver");
+		String titlename=driver.getTitle();
+		System.out.println("Page title name is " + titlename);
+	//	driver.navigate().to("https://www.google.com");
 		 
-		driver.navigate().back();
-		driver.navigate().refresh();
-		driver.wait(5000);
+		//driver.navigate().back();
+		//driver.navigate().refresh();
+		//driver.wait(5000);
 		// or use
 		// Thread.sleep(5000);
 		 
-		driver.findElement(By.name("email")).sendKeys("xxx@gmail.com");
-		driver.findElement(By.name("pass")).sendKeys("xxxxxx");
-		driver.findElement(By.id("u_0_q")).click();
+	//	driver.findElement(By.name("email")).sendKeys("xxx@gmail.com");
+		//driver.findElement(By.name("pass")).sendKeys("xxxxxx");
+		//driver.findElement(By.id("u_0_q")).click();
 		 
-		driver.quit();
+		//driver.quit();
 		}
 		
 
